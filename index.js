e35c19a9589c6d8e1cf4a980cb773e02cbb77853
martha-ivana <martha.syryca@gmail.com>
2022-04-13 10:55:07 -0400

    tested locally

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-export const annotationsByMsHashMap = {}
-export const maxVideoLengthInMs = 60 * 60 * 1000
+const annotationsByMsHashMap = {}
+const maxVideoLengthInMs = 60 * 60 * 1000
 
-export const convertTimestampToMs = (timestamp) => {
+const convertTimestampToMs = timestamp => {
   return (timestamp.getMinutes() * 60 * 1000) + (timestamp.getSeconds() * 1000)
 }
 
-exports.mapAnnotationsByMs = (tracks, videoLengthInMs) => {  
-  for (let i = 0; i < videoLengthInMs; i++) {
-    annotationsByMsHashMap[i] = { visible: [], hidden: []}
+const mapAnnotationsByMs = (tracks, videoLengthInMs) => {  
+  for (let i = 0; i <= videoLengthInMs; i++) {
+    console.log('i', i)
+    annotationsByMsHashMap[i] = { visible: [], hidden: [] }
     for (let j = 0; j < tracks.length; j++) {
       tracks[j].forEach(annotation => {
-        if (convertTimestampToMs(annotation.startTime) >= i && convertTimestampToMs(annotation.endTime) <= i) {
+        if (convertTimestampToMs(annotation.startTime) <= i && convertTimestampToMs(annotation.endTime) <= i) {
           annotationsByMsHashMap[i].visible.push(annotation)
         } else {
           annotationsByMsHashMap[i].hidden.push(annotation)
@@ -21,7 +22,12 @@ exports.mapAnnotationsByMs = (tracks, videoLengthInMs) => {
 }
 
 // this method gets called in timeUpdate
-exports.setAnnotations = (currentTimeInSec) => {
+const setAnnotations = (currentTimeInSec) => {
   annotationsByMsHashMap[currentTimeInSec * 1000].visible.forEach(annotation => annotation.show())
   annotationsByMsHashMap[currentTimeInSec * 1000].hidden.forEach(annotation => annotation.hide())
+}
+
+exports.handler = (tracks, currentTime) => {
+  mapAnnotationsByMs(tracks, maxVideoLengthInMs)
+  setAnnotations(currentTime)
 }
--- local.js
+++ local.js
@@ -1,41 +1,47 @@
 const index = require('./index')
 
-// test tracks -- not sure what timestamp data type looks like in this case
 const testTracks = [
   [{
-    startTime: new Date(2022, 4, 12, 22, 21, 0),
-    endTime: new Date(2022, 4, 12, 22, 21, 1),
+    id: 100,
+    startTime: new Date(2022, 4, 12, 22, 0, 0),
+    endTime: new Date(2022, 4, 12, 22, 30, 30),
     visible: false,
     show: () => { this.visible = true },
     hide: () => { this.visible = false }
   },
   {
-    startTime: new Date(2022, 4, 12, 21, 26, 23),
-    endTime: new Date(2022, 4, 12, 22, 6, 50),
+    id: 101,
+    startTime: new Date(2022, 4, 12, 22, 0, 23),
+    endTime: new Date(2022, 4, 12, 22, 5, 50),
     visible: false,
     show: () => { this.visible = true },
     hide: () => { this.visible = false }
   },
   {
-    startTime: new Date(2022, 4, 12, 21, 26, 23),
-    endTime: new Date(2022, 4, 12, 22, 6, 50),
-    visible: false,
-    show: () => { this.visible = true },
-    hide: () => { this.visible = false }
-  }]
-  [{
-    startTime: new Date(2022, 4, 12, 22, 23, 23),
+    id: 102,
+    startTime: new Date(2022, 4, 12, 22, 6, 12),
     endTime: new Date(2022, 4, 12, 22, 26, 50),
     visible: false,
     show: () => { this.visible = true },
     hide: () => { this.visible = false }
-  }]
+  }],
   [{
+    id: 200,
+    startTime: new Date(2022, 4, 12, 22, 23, 23),
+    endTime: new Date(2022, 4, 12, 22, 26, 33),
+    visible: false,
+    show: () => { this.visible = true },
+    hide: () => { this.visible = false }
+  }],
+  [{
+    id: 300,
     startTime: new Date(2022, 4, 12, 22, 11, 23),
-    endTime: new Date(2022, 4, 12, 22, 55, 50),
+    endTime: new Date(2022, 4, 12, 22, 55, 45),
     visible: false,
     show: () => { this.visible = true },
     hide: () => { this.visible = false }
   }]
 ]
 
+// test
+index.handler(testTracks, 240)
